fe237049e68d6613c683555aa80345984362496a
yoonys_laptop <yyd1109@gmail.com>
2022-02-26 10:44:57 +0900

    disp_sync_gen_fsm.sv update : comment update

--- src/disp_sync_gen_fsm.sv
+++ src/disp_sync_gen_fsm.sv
@@ -56,6 +56,8 @@ logic               vsync_d;
 logic     [1:0]     hsync_d;
 logic     [1:0]     de_d;
 
+
+// LUT : 49, FF:35
 // HSYNC 
 always @(posedge i_clk, negedge rst_n) begin
   if(~rst_n) begin 
@@ -150,6 +152,102 @@ end
 assign t_hsync  = (cur_Hstate == S_HPULSE ) && (clkCnt      <= HPULSE-1   );
 assign t_vsync  = (cur_Vstate == S_VPULSE ) && (hsyncCnt    <= VPULSE-1   );
 assign t_de     = (cur_Hstate == S_HACTIVE) && (cur_Vstate  == S_VACTIVE  );
+
+//// LUT : 53, FF:35
+//// HSYNC 
+//always @(posedge i_clk, negedge rst_n) begin
+//  if(~rst_n) begin 
+//    cur_Hstate  <= S_HIDLE;
+//  end else begin
+//    cur_Hstate  <= nxt_Hstate;
+//  end
+//end
+//
+//always @(*) begin
+//  nxt_Hstate = cur_Hstate; // default 
+//  case (cur_Hstate)
+//    S_HIDLE   : if(rst_n              ) nxt_Hstate = S_HPULSE;              
+//    S_HPULSE  : if(clkCnt == HPULSE   ) nxt_Hstate = S_HBP;
+//    S_HBP     : if(clkCnt == HBP      ) nxt_Hstate = S_HACTIVE;
+//    S_HACTIVE : if(clkCnt == HRES     ) nxt_Hstate = S_HFP;
+//    S_HFP     : if(clkCnt == HFP      ) nxt_Hstate = S_HPULSE;
+//  endcase
+//end
+//
+//always @(posedge i_clk, negedge rst_n ) begin
+//  if(~rst_n) begin
+//    clkCnt <= 'd1;
+//  end else begin
+//    case(cur_Hstate)
+//      S_HIDLE   : clkCnt <= clkCnt;
+//      S_HPULSE  : begin
+//        if(clkCnt == HPULSE)    clkCnt <= 'd1;
+//        else                    clkCnt <= clkCnt +'d1;
+//      end
+//      S_HBP     : begin
+//        if(clkCnt == HBP)       clkCnt <= 'd1;
+//        else                    clkCnt <= clkCnt +'d1;
+//      end
+//      S_HACTIVE : begin
+//        if(clkCnt == HRES)      clkCnt <= 'd1;
+//        else                    clkCnt <= clkCnt +'d1;
+//      end
+//      S_HFP     : begin
+//        if(clkCnt == HFP)       clkCnt <= 'd1;
+//        else                    clkCnt <= clkCnt +'d1;
+//      end
+//    endcase
+//  end
+//end
+//
+//// VSYNC 
+//always @(posedge i_clk, negedge rst_n) begin
+//  if(~rst_n) begin 
+//    cur_Vstate  <= S_VIDLE;
+//  end else if(cur_Hstate == S_HPULSE && clkCnt == 'd1) begin
+//    cur_Vstate  <= nxt_Vstate;
+//  end
+//end
+//always @(*) begin
+//  nxt_Vstate = cur_Vstate; // default 
+//  case (cur_Vstate)
+//    S_VIDLE   : if(rst_n                ) nxt_Vstate = S_VPULSE;              
+//    S_VPULSE  : if(hsyncCnt == VPULSE   ) nxt_Vstate = S_VBP;
+//    S_VBP     : if(hsyncCnt == VBP      ) nxt_Vstate = S_VACTIVE;
+//    S_VACTIVE : if(hsyncCnt == VRES     ) nxt_Vstate = S_VFP;
+//    S_VFP     : if(hsyncCnt == VFP      ) nxt_Vstate = S_VPULSE;
+//  endcase
+//end
+//
+//always @(posedge i_clk, negedge rst_n ) begin
+//  if(~rst_n) begin
+//    hsyncCnt <= 'd1;
+//  end else if(cur_Hstate == S_HPULSE && clkCnt == 'd1) begin
+//    case(cur_Vstate)
+//      S_VIDLE   : hsyncCnt <= hsyncCnt;
+//      S_VPULSE  : begin
+//        if(hsyncCnt == VPULSE)    hsyncCnt <= 'd1;
+//        else                      hsyncCnt <= hsyncCnt +'d1;
+//      end
+//      S_VBP     : begin
+//        if(hsyncCnt == VBP)       hsyncCnt <= 'd1;
+//        else                      hsyncCnt <= hsyncCnt +'d1;
+//      end
+//      S_VACTIVE : begin
+//        if(hsyncCnt == VRES)      hsyncCnt <= 'd1;
+//        else                      hsyncCnt <= hsyncCnt +'d1;
+//      end
+//      S_VFP     : begin
+//        if(hsyncCnt == VFP)       hsyncCnt <= 'd1;
+//        else                      hsyncCnt <= hsyncCnt +'d1;
+//      end
+//    endcase
+//  end
+//end
+//
+//assign t_hsync  = (cur_Hstate == S_HPULSE ) && (clkCnt      <= HPULSE     );
+//assign t_vsync  = (cur_Vstate == S_VPULSE ) && (hsyncCnt    <= VPULSE     );
+//assign t_de     = (cur_Hstate == S_HACTIVE) && (cur_Vstate  == S_VACTIVE  );
 
 always @(posedge i_clk, negedge rst_n) begin
   if(~rst_n) begin 
