62220d7e43c4cff2591b442e319722b83cebf027
yoonys_laptop <yyd1109@gmail.com>
2022-03-02 20:50:31 +0900

    pkg.sv file added

--- src/disp_sync_gen_fsm.sv
+++ src/disp_sync_gen_fsm.sv
@@ -1,3 +1,5 @@
+import state_pkg::*;
+
 module disp_sync_gen_fsm 	  #(
 //========================================== 
 // Parameter Description
@@ -23,21 +25,21 @@ parameter HFP     = 5   )(
 localparam V_TOTAL = VPULSE + VBP + VRES + VFP;
 localparam H_TOTAL = HPULSE + HBP + HRES + HFP; 
 
-typedef enum logic[2:0] {
-  S_VIDLE,  
-  S_VPULSE,
-  S_VBP,
-  S_VACTIVE,
-  S_VFP
-} Vstate_t;
-
-typedef enum logic[2:0] {
-  S_HIDLE,  
-  S_HPULSE,
-  S_HBP,
-  S_HACTIVE,
-  S_HFP
-} Hstate_t;
+//typedef enum logic[2:0] {
+//  S_VIDLE,  
+//  S_VPULSE,
+//  S_VBP,
+//  S_VACTIVE,
+//  S_VFP
+//} Vstate_t;
+//
+//typedef enum logic[2:0] {
+//  S_HIDLE,  
+//  S_HPULSE,
+//  S_HBP,
+//  S_HACTIVE,
+//  S_HFP
+//} Hstate_t;
 
 //========================================== 
 // Internal Signal Description
